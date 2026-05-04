28a5ed9e20c8e3dd62e84cae4866b8ee482fdac8
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 05:22:59 +0530

    Add examples of assignment operators in Python
    Demonstrate various assignment operators with print statements.

---
+++ assignment_operators.py
@@ -0,0 +1,15 @@
+#assignment operators
+
+num = 10
+num += 5 #performs addition
+print("num : ",num) #15
+num -= 5
+print("num : ",num)#5
+num *= 5
+print("num : ",num)#50
+num /= 5
+print("num : ",num)#2
+num %= 5
+print("num : ",num)#0
+num **= 2
+print("num : ",num)#100
