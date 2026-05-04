39f8d7d681950e4bbbebd368396dfefe1b1f1f44
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 05:06:02 +0530

    Add Comments.py with example comments

---
+++ Comments.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+#This is a single line comment
+
+'''This is
+a multi - line
+comment'''
+
+# print("Hello World")  [CTRL + /] - comments out multiple lines at once as single line comments
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
