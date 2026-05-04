cfedbb179b7de633164914d6f251663833449c67
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 05:28:13 +0530

    Add logical operators demonstration in logical_operators.py

---
+++ logical_operators.py
@@ -0,0 +1,12 @@
+#logical operators
+
+a = 50
+b = 20
+print(not False) #True
+print(not(a > b)) #False 
+
+val1 = True
+val2 = False
+print("AND operator : ",val1 and val2) #False
+print("OR operator : ",(a == b) or (a > b)) #True
+print("ans of operator : ",not val2) #True
