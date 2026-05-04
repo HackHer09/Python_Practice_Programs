5d23fe03e23c127fd17a99a25567978ada28f0da
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 05:15:04 +0530

    Add Arithmetic Operators example in Python
    Implement basic arithmetic operations in Python.

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,15 @@
+#arithmetic operators
+
+a = 2
+b = 8
+
+sum =  a + b # returns sum
+print(sum)   #prints after saving sum in a variable
+
+#Alternate method - print sum directly print(a + b)
+
+print(a - b) #returns difference
+print(a * b) #returns product
+print(b / a) #returns quotient
+print(b % a) #returns remainder
+print(b ** a) #returns b^a
