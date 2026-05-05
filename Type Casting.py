1788581dab291b609d2048e49ebd9e7b357a31c8
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 19:15:57 +0530

    Create Type Casting.py for explicit conversion example
    Add a script demonstrating type casting in Python.

---
+++ Type Casting.py
@@ -0,0 +1,10 @@
+#type casting
+
+a = 1
+b = "2"
+
+#This conversion is a forced conversion/explicit conversion
+
+c = int(b) # converts string 2 to int 2 and addition is performed 
+sum = a + c # 1 + 2 = 3
+print(sum)
