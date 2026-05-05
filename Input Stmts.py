a7cc768bb3c0b2a94b2eb20d7458eb707c29ee28
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-05 19:50:22 +0530

    Enhance Input Stmts.py with user input examples
    Added multiple input statements to capture user data.

---
+++ Input Stmts.py
@@ -0,0 +1,17 @@
+name = input("enter your age: ")
+print("you entered",name) #returns inputed name value
+
+val = input("enter some value: ")
+print(type(val),val) # returns value and entered value type
+
+int("5")
+val = float(input("enter some value : "))
+print(type(val),val)
+
+name = input("enter name: ")
+age = input("enterr age : ")
+marks = input("enter marks : ")
+
+print("Wecome ", name)
+print("age =", age)
+print("marks =",marks)
