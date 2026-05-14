05ae2d2352150a9680c2b34f19fb22f5c8cb4f9d
Ananya Beckwoad <anu.ananya.beckwoad@gmail.com>
2026-05-14 18:43:53 +0530

    Add files via upload

---
+++ PP3_flt_pt_avg.py
@@ -0,0 +1,7 @@
+a = float(input("Enter a number : "))
+b = float(input("Enter a number : "))
+
+print("The average is : ",(a+b)/2)
+
+# Or avg = (a+b)/2
+# print(avg)
---
+++ PP4_big_small.py
@@ -0,0 +1,7 @@
+a = float(input("Enter a number : "))
+b = float(input("Enter a number : "))
+
+if (a>b) :
+    print(a ," is bigger than ", b)
+else :
+    print(b ," is bigger than ", a)
